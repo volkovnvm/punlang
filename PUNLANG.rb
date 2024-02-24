3b0005c73cd40d950286a2729a9531d83d45bc44
VolkovN <161017258+volkovnvm@users.noreply.github.com>
2024-02-24 17:17:38 +0500

    Add files via upload

---
+++ PUNLANG.rb
@@ -0,0 +1,20 @@
+a = ""
+while a != "stop int" do
+  print "ВВЕДИ КОМАНДУ СУЧАРА\n"
+  a = gets.chomp
+  i = 1
+  print "ОТВЕТ КОМАНДЫ:\n"
+  if a == "kudah LMAO nonstop" then
+    while i < 10000
+      print "LMAO "
+      i+=1
+    end
+  elsif a == "xd hehe lol" then
+      print "xd hehe lol\n"
+  elsif a == "call Mundo" then
+      print "МУНДО ХОДИТЬ КУДА ХОТЕТЬ\n"
+  elsif a == "stop int" then
+    print "конец программы"
+  else print "ERROR OF CODE (МАРТЫН ЕЩЁ НЕ НАУЧИЛСЯ ЭТО ДЕЛАТЬ)\n"
+  end
+end
